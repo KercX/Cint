c46f426ff5854813f144843bb3cc4081b87d36a6
Brat1shkaKERCIX <sasaslobodan174@gmail.com>
2026-05-18 15:44:10 +0300

    Create init.lua

---
+++ src/init.lua
@@ -0,0 +1,11 @@
+local Cint = {}
+
+Cint.Version = "1.0.0"
+
+local Registry = require(script.Registry)
+local Loader = require(script.Loader)
+
+Cint.Registry = Registry
+Cint.Loader = Loader
+
+return Cint
